5f0d68a957c63be756bdb47ea34be22e2c7cb147
MACBOOK <macbook@MacBook-Air-MACBOOK.local>
2026-04-14 21:37:19 +0700

    update pdf

--- resources/views/admin/reports/kebakaran_pdf.blade.php
+++ resources/views/admin/reports/kebakaran_pdf.blade.php
@@ -38,7 +38,7 @@
             position: absolute;
             left: 0;
             top: 0;
-            width: 150px;
+            width: 120px;
             height: auto;
         }
 
@@ -46,7 +46,7 @@
             position: absolute;
             right: 0;
             top: 0;
-            width: 190px;
+            width: 150px;
             height: auto;
         }
 
